5e2e7c2ca490a89babdd02c442d4d409425b2e5b
AnakinM <mateusz.anikiej@gmail.com>
2022-08-01 18:59:57 +0200

    singleton nested class implementation

---
+++ singleton/singleton_nested_class.py
@@ -0,0 +1,27 @@
+# Nested class implementation
+
+class Singleton:
+    class __Singleton:
+        def __init__(self):
+            self.val = 0
+
+    __instance = None
+
+    def __new__(cls):
+        if not Singleton.__instance:
+            Singleton.__instance = Singleton.__Singleton()
+        return Singleton.__instance
+
+
+if __name__ == '__main__':
+    x = Singleton()
+    print(x.val)
+    x.val = 3
+    print(x.val)
+
+    y = Singleton()
+    print(y.val)
+
+    print(x)
+    print(y)
+
