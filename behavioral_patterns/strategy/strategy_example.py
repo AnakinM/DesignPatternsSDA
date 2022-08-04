63ff25f22590b95fb691ada715a4b0ed8ea40b1d
AnakinM <mateusz.anikiej@gmail.com>
2022-08-04 18:51:44 +0200

    strategy example fixed

--- behavioral_patterns/strategy/strategy_example.py
+++ behavioral_patterns/strategy/strategy_example.py
@@ -1,62 +1,120 @@
+"""Strategy pattern example"""
 import random
 from abc import ABC, abstractmethod
+from typing import Optional
 
 
-class MatureLanguageFilterStrategy(ABC):
+class MatureLanguageFilterStrategy(ABC): # pylint: disable=too-few-public-methods
+    """MatureLanguageFilterStrategy class"""
     @abstractmethod
     def modify(self, text: str, word: str) -> str:
-        pass
+        """ Modify method that applies modification to a given
+        test.
+
+        :param text:
+        :param word:
+        :return:
+        """
 
 
-class RemoveWordStrategy(MatureLanguageFilterStrategy):
+class RemoveWordStrategy(MatureLanguageFilterStrategy): # pylint: disable=too-few-public-methods
+    """RemoveWordStrategy class"""
     def modify(self, text: str, word: str) -> str:
+        """ Modify method that applies modification to a given
+        test.
+
+        :param text:
+        :param word:
+        :return:
+        """
         return text.replace(word, "")
 
 
-class StarWordStrategy(MatureLanguageFilterStrategy):
+class StarWordStrategy(MatureLanguageFilterStrategy): # pylint: disable=too-few-public-methods
+    """RemoveWordStrategy class"""
     def modify(self, text: str, word: str) -> str:
+        """ Modify method that applies modification to a given
+        test.
+
+        :param text:
+        :param word:
+        :return:
+        """
         return text.replace(word, "*" * len(word))
 
 
-class MessWordStrategy(MatureLanguageFilterStrategy):
+class MessWordStrategy(MatureLanguageFilterStrategy): # pylint: disable=too-few-public-methods
+    """RemoveWordStrategy class"""
     def modify(self, text: str, word: str) -> str:
+        """ Modify method that applies modification to a given
+        test.
+
+        :param text:
+        :param word:
+        :return:
+        """
         letters = list(word)
         random.shuffle(letters)
         return text.replace(word, "".join(letters))
 
 
-class BackwardStrategy(MatureLanguageFilterStrategy):
+class BackwardStrategy(MatureLanguageFilterStrategy): # pylint: disable=too-few-public-methods
+    """RemoveWordStrategy class"""
     def modify(self, text: str, word: str) -> str:
+        """ Modify method that applies modification to a given
+        test.
+
+        :param text:
+        :param word:
+        :return:
+        """
         return text.replace(word, word[::-1])
 
 
-class CensoreMiddleWordStrategy(MatureLanguageFilterStrategy):
+class CensoreMiddleWordStrategy(MatureLanguageFilterStrategy): # pylint: disable=too-few-public-methods
+    """RemoveWordStrategy class"""
     def modify(self, text: str, word: str) -> str:
+        """ Modify method that applies modification to a given
+        test.
+
+        :param text:
+        :param word:
+        :return:
+        """
         censored_word = word[0] + "*" * (len(word) - 2) + word[-1]
         return text.replace(word, censored_word)
 
 
-class MatureLanguageFilterStrategyProvider:
+class MatureLanguageFilterStrategyProvider: # pylint: disable=too-few-public-methods
+    """RemoveWordStrategy class"""
     @staticmethod
-    def get_strategy(strategy_type: str) -> MatureLanguageFilterStrategy:
+    def get_strategy(
+            strategy_type: str
+    ) -> Optional[MatureLanguageFilterStrategy]:
+        """ Method that provides requested strategy.
+
+        :param strategy_type:
+        :return:
+        """
         if strategy_type.lower() == "remove":
             return RemoveWordStrategy()
-        elif strategy_type.lower() == "star":
+        if strategy_type.lower() == "star":
             return StarWordStrategy()
-        elif strategy_type.lower() == "mess":
+        if strategy_type.lower() == "mess":
             return MessWordStrategy()
-        elif strategy_type.lower() == "backward":
+        if strategy_type.lower() == "backward":
             return BackwardStrategy()
-        elif strategy_type.lower() == "partcensore":
+        if strategy_type.lower() == "partcensore":
             return CensoreMiddleWordStrategy()
+        return None
 
 
 if __name__ == "__main__":
     choice = input("Choose strategy [remove|star|mess|backward|partcensore]: ")
-    text_to_filter = "Holy coaww now I have to coaww go there myself!"
+    TEXT_TO_FILTER = "Holy coaww now I have to coaww go there myself!"
 
     strategy = MatureLanguageFilterStrategyProvider().get_strategy(choice)
-    output = strategy.modify(text_to_filter, "coaww")
+    output = strategy.modify(TEXT_TO_FILTER, "coaww")
     print(output)
 
 # Zadanie 1
